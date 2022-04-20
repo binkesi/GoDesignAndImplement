ea44b44bc3fb4690aea789fab2879c045b88827f
binkesi <sjtusgn@163.com>
2022-04-20 14:29:01 +0800

    practise fan in

--- ConcurrencyCoding/pipline_test.go
+++ ConcurrencyCoding/pipline_test.go
@@ -2,11 +2,22 @@ package concurrencycoding
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 )
 
 func TestPipeline(t *testing.T) {
 	for num := range sq(sq(gen(2, 3, 4))) {
+		fmt.Println(num)
+	}
+}
+
+func TestMergePipeline(t *testing.T) {
+	ch := gen(2, 3, 4)
+	sq1 := sq(ch)
+	sq2 := sq(ch)
+	out := merge(sq1, sq2)
+	for num := range out {
 		fmt.Println(num)
 	}
 }
@@ -28,6 +39,26 @@ func sq(in chan int) chan int {
 		for num := range in {
 			out <- num * num
 		}
+		close(out)
+	}()
+	return out
+}
+
+func merge(ch ...chan int) <-chan int {
+	wg := sync.WaitGroup{}
+	out := make(chan int)
+	output := func(c <-chan int) {
+		for num := range c {
+			out <- num
+		}
+		wg.Done()
+	}
+	wg.Add(len(ch))
+	for _, in := range ch {
+		go output(in)
+	}
+	go func() {
+		wg.Wait()
 		close(out)
 	}()
 	return out
